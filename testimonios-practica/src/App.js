d3517e8fee5b79f821bcc57c9c168d6b2b2a90ad
Ezequiel-valdiviezo <ezequiel.valdiviezo02@gmail.com>
2023-12-30 18:02:04 -0300

    detalle en practica de testimonios

--- testimonios-practica/src/App.js
+++ testimonios-practica/src/App.js
@@ -10,7 +10,7 @@ function App() {
       imagen: 'shawn',
       cargo: 'Ingeniero de software',
       empresa: 'Amazon',
-      testimonio: 'Da miedo cambiar de carrera. Solo gané confianza en que podía codificar trabajando durante cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
+      testimonio: 'Da miedo cambiar de carrera. Solo gané confianza en que podía codificar trabajando durante cientos de horas de lecciones gratuitas. En un año tenía un trabajo de seis cifras como ingeniero de software.'
     },
     {
       nombre: 'Sara Chima',
@@ -18,7 +18,7 @@ function App() {
       imagen: 'sarah',
       cargo: 'Ingeniero de software',
       empresa: 'ChatDesk',
-      testimonio: 'freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de codificación desde un nivel principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollo en una empresa increíble.'
+      testimonio: 'Esto fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de codificación desde un nivel principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollo en una empresa increíble.'
     },
     {
       nombre: 'Emma Bostian',
@@ -26,14 +26,14 @@ function App() {
       imagen: 'emma',
       cargo: 'Ingeniero de software',
       empresa: 'Spotify',
-      testimonio: 'Siempre me ha costado aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+      testimonio: 'Siempre me ha costado aprender JavaScript. He tomado muchos cursos, pero este curso fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en el curso me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
     }
   ];
 
   return (
     <div className="App">
       <div className='contenedor-principal'>
-        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <h1>Esto es lo que dicen nuestros alumnos sobre nosotros:</h1>
         {testimoniosData.map((testimonio, index) => (
           <Testimonio key={index} {...testimonio} />
         ))}
